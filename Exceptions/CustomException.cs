09854a28d84819659a5ea032c594ac92e45e6663
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-14 20:56:42 +0200

    Add CustomException class with enhanced logging
    Introduce a new `CustomException` class in the `GtvApiHub.Exceptions` namespace, extending the base `Exception` class. This class includes additional logging functionality using `ILogger<CustomException>`, initialized via a static constructor with NLog. Two constructors are provided: one for a message and optional caller info, and another for a message, inner exception, and optional caller info. Both constructors call a private `LogException` method to log details, including the exception message and inner exception if available. Caller attributes capture the source of the exception.

---
+++ Exceptions/CustomException.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System.Runtime.CompilerServices;
+
+namespace GtvApiHub.Exceptions
+{
+    public class CustomException : Exception
+    {
+        private static readonly ILogger<CustomException> _logger;
+
+        static CustomException()
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.ClearProviders();
+                builder.SetMinimumLevel(LogLevel.Trace);
+                builder.AddNLog();
+            });
+            _logger = loggerFactory.CreateLogger<CustomException>();
+        }
+
+        public CustomException(string message,
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string memberName = "")
+            : base($"File: {filePath}|Line: {lineNumber}|Member: {memberName}|message: {message}")
+        {
+            LogException(message, null, filePath, lineNumber, memberName);
+        }
+
+        public CustomException(string message, Exception inner,
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string memberName = "")
+            : base($"{message} (File: {filePath}, Line: {lineNumber}, Member: {memberName})", inner)
+        {
+            LogException(message, inner, filePath, lineNumber, memberName);
+        }
+
+        private void LogException(string message, Exception? innerException, string filePath, int lineNumber, string memberName)
+        {
+            var logMessage = $"{message} (File: {filePath}, Line: {lineNumber}, Member: {memberName})";
+            if (innerException != null)
+            {
+                _logger.LogError(innerException, logMessage);
+            }
+            else
+            {
+                _logger.LogError(logMessage);
+            }
+        }
+    }
+}
