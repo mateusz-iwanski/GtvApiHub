e32703674ba9a0eced336a5a02508b084a01bbac
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-14 20:57:24 +0200

    Add ApiConfiguration class for logging and HTTP setup
    Introduced a new class `ApiConfiguration` in the `GtvApiHub.WebApi` namespace to handle logging and HTTP client configuration. Key changes include:
    
    - Imported necessary namespaces for logging, configuration, and HTTP handling.
    - Added protected properties for `logger`, `httpClientHandler`, `customHandler`, and `httpClient`.
    - Implemented a constructor that sets up the logger using NLog, initializes HTTP client handlers, and configures the `HttpClient` with a base address from `appsettings.json`.

---
+++ WebApi/ApiConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using GtvApiHub.Exceptions;
+using GtvApiHub.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NLog;
+using NLog.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi
+{
+    public class ApiConfiguration
+    {
+        protected Logger logger { get; set; }
+        protected HttpClientHandler httpClientHandler { get; set; }
+        protected CustomHttpClientHandler customHandler { get; set; }
+        protected HttpClient httpClient { get; set; }
+
+        public ApiConfiguration(IOptions<GtvApiSettings> gtvApiSettings)
+        {
+            // get configuration from appsettings.json
+            LogManager.Setup().LoadConfigurationFromAppSettings();
+            logger = LogManager.GetCurrentClassLogger();
+            httpClientHandler = new HttpClientHandler();
+            customHandler = new CustomHttpClientHandler(httpClientHandler, logger);
+
+            string Uri = gtvApiSettings.Value.Url;
+
+            httpClient = new HttpClient(customHandler)
+            {
+                BaseAddress = new Uri(Uri)
+            };
+        }
+    }
+}
