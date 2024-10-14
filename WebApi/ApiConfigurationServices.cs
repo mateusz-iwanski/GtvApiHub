100655c982b730a8722ed20a290e19dd48805e1d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-14 20:57:37 +0200

    Add IApiConfigurationServices interface and implementation
    Introduced IApiConfigurationServices interface with TokenService property.
    Created ApiConfigurationServices class inheriting from ApiConfiguration
    and implementing IApiConfigurationServices. Initialized TokenService
    using RestService.For<ITokenService>(httpClient) in the constructor.
    Added necessary namespaces for the new functionality.

---
+++ WebApi/ApiConfigurationServices.cs
@@ -0,0 +1,29 @@
+﻿using GtvApiHub.WebApi.EndPointInterfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using NLog;
+using NLog.Web;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi
+{
+    public interface IApiConfigurationServices
+    {
+        ITokenService TokenService { get; }
+    }
+
+    public class ApiConfigurationServices : ApiConfiguration, IApiConfigurationServices
+    {
+        public ITokenService TokenService { get; private set; }
+
+        public ApiConfigurationServices(IOptions<GtvApiSettings> gtvApiSettings) : base(gtvApiSettings)
+        {
+            TokenService = RestService.For<ITokenService>(httpClient);
+        }
+    }
+}
