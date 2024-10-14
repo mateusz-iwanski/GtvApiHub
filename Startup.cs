e3df05537b8d3648d708960a89de39714b93f9bb
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-14 11:38:30 +0200

    Add Startup class to configure services and NLog logging
    Introduced a new `Startup` class in the `GtvApiHub` namespace to
    configure services for an ASP.NET Core application. The
    `ConfigureServices` method registers a singleton `IConfiguration`
    service that reads from `settings.json`. NLog is configured as the
    logging provider by clearing existing providers, setting the minimum
    log level to `Trace`, and registering NLog's `ILogger` as a singleton
    service.

---
+++ Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace GtvApiHub
+{
+    /// <summary>
+    /// Represents a builder for configuring services.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the ConfigureServices class.
+        /// </summary>
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<IConfiguration>((ConfigurationBuilder) =>
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("settings.json", optional: false, reloadOnChange: true)
+                    .Build();
+            });
+
+            // Add NLog as the logging provider
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders(); // Clear any existing logging providers
+                loggingBuilder.SetMinimumLevel(LogLevel.Trace); // Set the minimum log level
+
+                // Add NLog
+                loggingBuilder.AddNLog();
+            });
+            // Register NLog's ILogger
+            services.AddSingleton<NLog.ILogger>(provider => NLog.LogManager.GetCurrentClassLogger());
+        }
+    }
+}
