359f1eea0ce2c6320b046bc332d8624098663338
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-14 20:58:31 +0200

    Add TokenSettings class to manage token-related settings
    Introduced a new namespace `GtvApiHub.WebApi` and added a new
    public class `TokenSettings` within it. This class includes
    private properties `_secretToken` and `_expiresIn` with
    corresponding public properties `SecretToken` and `ExpiresIn`.
    Also added necessary using directives and commented out
    exception handling for missing settings in `appsettings.json`.

---
+++ WebApi/TokenSettings.cs
@@ -0,0 +1,27 @@
+﻿using GtvApiHub.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi
+{
+    public class TokenSettings
+    {
+        private string? _secretToken { get; set; }
+        private string? _expiresIn { get; set; }
+
+        public string? SecretToken
+        {
+            get { return _secretToken; } ///?? throw new SettingsException($"In appsettings.json TokenSettings->SecretToken not exists"); }
+            set { _secretToken = value; }
+        }
+
+        public string? ExpiresIn
+        {
+            get { return _expiresIn; } //?? throw new SettingsException($"In appsettings.json TokenSettings->ExpiresIn not exists"); }
+            set { _expiresIn = value; }
+        }
+    }
+}
