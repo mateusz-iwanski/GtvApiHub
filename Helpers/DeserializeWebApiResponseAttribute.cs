c2c88c846b671c08211a636b2d9229c4c54b8f15
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 13:23:24 +0200

    Add DeserializeWebApiResponseAttribute for API response handling
    Introduce DeserializeWebApiResponseAttribute class in DeserializeWebApiResponseAttribute.cs for deserializing Web API responses. Added XML documentation, using directive for System namespace, and AttributeUsage to specify method application and inheritance.

---
+++ Helpers/DeserializeWebApiResponseAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// <c>DeserializeWebApiResponseAttribute</c> is used to deserialize the response from the Web Api server
+/// </summary>
+/// <remarks>
+/// It works with the DeserializeWebApiResponseAsync.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class DeserializeWebApiResponseAttribute : Attribute
+{
+}
