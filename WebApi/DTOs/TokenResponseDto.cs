98f5d94d3b05d5a7a352a13b78a0b21260a00d81
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 13:24:57 +0200

    Add TokenResponseDto class for token response handling
    Introduced a new record class `TokenResponseDto` in the `GtvApiHub.WebApi.Objects` namespace. This class implements `IBaseDto` and `IResponseDto` interfaces and includes properties `TokenType`, `AccessToken`, and `ExpiresIn`, decorated with `JsonProperty` attributes for JSON serialization. Added necessary using directives for `Newtonsoft.Json`, `Refit`, and other namespaces.

---
+++ WebApi/DTOs/TokenResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using Newtonsoft.Json;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Objects
+{
+    /// <summary>
+    /// Token request object.
+    /// </summary>
+    /// <param name="expires_in">Date when token expires</param>
+    public record TokenResponseDto
+    (
+        [property: JsonProperty("token_type")] string TokenType,
+        [property: JsonProperty("access_token")] string AccessToken,
+        [property: JsonProperty("expires_in")] DateTime ExpiresIn
+    ) : IBaseDto, IResponseDto;
+}
