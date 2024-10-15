b2b05448024d241539c14be682543ea972420394
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 09:55:05 +0200

    Add Token class with async token retrieval logic
    Introduced multiple using directives and a new namespace `GtvApiHub.WebApi`. Created a `Token` class implementing `IToken` interface with private readonly fields `_tokenService`, `_tokenSettingsManager`, `_username`, and `_password`. Implemented a constructor for dependency injection. Added `GetTokenAsync` method to handle token expiration, request new tokens, deserialize responses, update settings, and return `TokenResponseDto`.

---
+++ WebApi/Token.cs
@@ -0,0 +1,63 @@
+﻿using GtvApiHub.Exceptions;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Objects;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi
+{
+    public class Token : IToken
+    {
+        private readonly ITokenService _tokenService;
+        private readonly ITokenSettingsManager _tokenSettingsManager;
+
+        private readonly string _username;
+        private readonly string _password;
+
+        public Token(IApiConfigurationServices services, IOptions<GtvApiSettings> gtvApiSettings, ITokenSettingsManager tokenSettingsManager)
+        {
+            _tokenService = services.TokenService;
+            _tokenSettingsManager = tokenSettingsManager;
+
+            _username = gtvApiSettings.Value.Username;
+            _password = gtvApiSettings.Value.Password;
+        }
+
+        /// <summary>
+        /// Get token from the API.
+        /// 
+        /// The token has an expiration date.
+        /// </summary>
+        /// <returns>TokenResponseDto or null if this is a problem</returns>
+        [DeserializeWebApiResponse]
+        public async Task<TokenResponseDto>? GetTokenAsync()
+        {
+            // first check if the token is not expired
+            if (checkExpirationDate())
+                return new TokenResponseDto
+                (
+                    "",
+                    _tokenSettingsManager.GetTokenSettings().SecretToken,
+                    DateTime.Parse(_tokenSettingsManager.GetTokenSettings().ExpiresIn)
+                );
+
+            // if expired request for a new one
+            var token = await _tokenService.GetAsync(new TokenRequestDto { Username = _username, Password = _password });
+            var tokenResponse = await token.Content.ReadAsStringAsync();
+            var deserializedResponse = JsonConvert.DeserializeObject<TokenResponseDto>(tokenResponse);
+
+            // write token to settings file
+            _tokenSettingsManager.UpdateTokenSettings(deserializedResponse);
+
+            return deserializedResponse;
+        }
+
+    }
+}
