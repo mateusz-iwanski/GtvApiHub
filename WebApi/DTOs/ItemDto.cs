66777a193822db02c33011052270404616f09127
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 13:24:25 +0200

    Add ItemDto record for API products
    Introduce ItemDto.cs with necessary using directives and define the ItemDto record in the GtvApiHub.WebApi.DTOs namespace. The record includes properties for ItemCode, ItemName, and LanguageCode, and implements IBaseDto and IResponseDto interfaces. Added XML documentation for clarity.

---
+++ WebApi/DTOs/ItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.DTOs
+{
+    /// <summary>
+    /// Item DTO represents products from Api.
+    /// </summary>
+    /// <param name="ItemCode">Symbol</param>
+    /// <param name="ItemName">Ttile</param>
+    /// <param name="LanguageCode">Language code</param>
+    public record ItemDto(string ItemCode, string ItemName, string LanguageCode) : IBaseDto, IResponseDto;
+}
