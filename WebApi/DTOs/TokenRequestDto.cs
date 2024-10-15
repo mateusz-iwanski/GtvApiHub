ba342142c4aae02a9f4e9d45f8c614e5a40cec37
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 13:24:45 +0200

    Add TokenRequestDto class for token request handling
    A new class `TokenRequestDto` has been added to the `GtvApiHub.WebApi.Objects` namespace. This class implements the `IBaseDto` interface and includes three properties: `Username`, `Password`, and a read-only `GrantType` that always returns `"password"`. Each property is annotated with `[AliasAs]` attributes. The class is designed to serve as a request object for token responses. Necessary using directives have also been added.

---
+++ WebApi/DTOs/TokenRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Objects
+{
+    /// <summary>
+    /// Reqest object for a token response.
+    /// </summary>
+    public class TokenRequestDto : IBaseDto
+    {
+        [AliasAs("username")]
+        public string Username { get; set; }
+
+        [AliasAs("password")]
+        public string Password { get; set; }
+
+        [AliasAs("grant_type")]
+        public string GrantType { get => "password"; }
+
+    }
+
+}
