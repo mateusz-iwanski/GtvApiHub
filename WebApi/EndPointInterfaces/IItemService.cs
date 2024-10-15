71813eeb775e27631219ea8a7ec63c87e76792ef
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 13:25:13 +0200

    Add IItemService interface for product representation
    Introduced a new interface `IItemService` in the `GtvApiHub.WebApi.EndPointInterfaces` namespace. This interface includes a method `GetAsync` that uses the Refit library to make an HTTP GET request to the endpoint `/odata/item`. Added necessary using directives for `Refit`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ WebApi/EndPointInterfaces/IItemService.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    /// <summary>
+    /// Item interface.
+    /// 
+    /// Represents product.
+    /// </summary>
+    public interface IItemService
+    {
+        [Get("/odata/item")]
+        Task<HttpResponseMessage> GetAsync();
+    }
+}
