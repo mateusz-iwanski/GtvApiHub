e7059e68bde5ee7e000889f61f9d161abd15863c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 13:22:01 +0200

    Add SettingsException class to handle specific settings errors
    A new file `SettingsException.cs` is created to introduce the
    `SettingsException` class in the `GtvApiHub.Exceptions` namespace.
    This class inherits from `CustomException` and includes two
    constructors: one for a message string and another for a message
    string and an inner exception. Necessary namespaces are imported.

---
+++ Exceptions/SettingsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.Exceptions
+{
+    public class SettingsException : CustomException
+    {
+        public SettingsException(string message) : base(message)
+        {
+        }
+
+        public SettingsException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
