1bed1ffdeb9de2b1d92246db01323db3bb50cde6
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-15 13:25:26 +0200

    Add ITokenService interface for OAuth 2.0 token requests
    Introduced a new namespace `GtvApiHub.WebApi.EndPointInterfaces`.
    Created `ITokenService` interface with a summary comment.
    Added `GetAsync` method for POST requests to `/connect/token`.
    Included necessary using directives for various namespaces.

---
+++ WebApi/EndPointInterfaces/ITokenService.cs
@@ -0,0 +1,23 @@
+﻿using GtvApiHub.Helpers;
+using GtvApiHub.WebApi.Objects;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    /// <summary>
+    /// Token interface.
+    /// 
+    /// Represents a token which is in the request header (OAuth 2.0).
+    /// </summary>
+
+    public interface ITokenService
+    {
+        [Post("/connect/token")]
+        Task<HttpResponseMessage> GetAsync([Body(BodySerializationMethod.UrlEncoded)] TokenRequestDto tokenCreateDto);
+    }
+}
