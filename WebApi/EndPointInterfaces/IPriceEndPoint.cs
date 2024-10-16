e2b1a3c46d93e1deac6cb94b6f03f330ecffd9b1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-16 16:22:27 +0200

    Add IPriceEndPoint interface for price-related endpoints
    Added a new interface `IPriceEndPoint` in the `GtvApiHub.WebApi.EndPointInterfaces` namespace. Imported necessary namespaces: `Refit`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`. Defined two methods within the `IPriceEndPoint` interface: `GetAsync()` for GET requests to `/odata/price`, and `GetByItemCodeAsync(string ItemCode)` for GET requests to `/odata/Price?$filter=ItemCode eq '{ItemCode}'`.

---
+++ WebApi/EndPointInterfaces/IPriceEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    public interface IPriceEndPoint
+    {
+        [Get("/odata/price")]
+        Task<HttpResponseMessage> GetAsync();
+
+        [Get("/odata/Price?$filter=ItemCode eq '{ItemCode}'")]
+        Task<HttpResponseMessage> GetByItemCodeAsync([AliasAs("ItemCode")] string ItemCode);
+    }
+}
