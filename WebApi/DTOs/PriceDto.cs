43a3f98bf06c9dcac1688ed4473ec17f1aca374a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-16 16:21:25 +0200

    Add PriceDto class to encapsulate pricing information
    Introduced a new `PriceDto` class in the `GtvApiHub.WebApi.DTOs` namespace. This class is a record and implements `IBaseDto` and `IResponseDto` interfaces. It includes properties for `CardCode`, `ItemCode`, `BasePrice`, `FinalPrice`, `Discount`, and `Currency`. Added necessary namespaces to support this functionality.

---
+++ WebApi/DTOs/PriceDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.DTOs
+{
+    public record PriceDto(
+        string CardCode, 
+        string ItemCode, 
+        decimal BasePrice, 
+        decimal FinalPrice, 
+        double Discount, 
+        string Currency
+        ) : IBaseDto, IResponseDto;
+}
