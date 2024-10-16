37f16a67219add281a612ba5d1a104ebcb73371c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-16 16:23:50 +0200

    Add IAttribute interface with async methods
    Introduce IAttribute interface in GtvApiHub.WebApi.Services.
    Define three async methods: GetAsync, GetByItemCodeAsync, and
    GetByLanguageCodeAsync. Import AttributeDto from GtvApiHub.WebApi.DTOs.

---
+++ WebApi/Services/IAttribute.cs
@@ -0,0 +1,11 @@
+﻿using GtvApiHub.WebApi.DTOs;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public interface IAttribute
+    {
+        Task<IEnumerable<AttributeDto>> GetAsync();
+        Task<IEnumerable<AttributeDto>> GetByItemCodeAsync(string itemCode);
+        Task<IEnumerable<AttributeDto>> GetByLanguageCodeAsync(LanguageCode languageCode);
+    }
+}
