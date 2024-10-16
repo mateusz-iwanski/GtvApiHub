419a271de5c31c8bafb89850c15c8311b289fc9c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-16 16:22:59 +0200

    Add AttributeItem service class with async data fetch methods
    The new AttributeItem class in the GtvApiHub.WebApi.Services namespace implements the IAttribute interface. It provides three asynchronous methods: GetAsync(), GetByItemCodeAsync(string itemCode), and GetByLanguageCodeAsync(LanguageCode languageCode). These methods fetch attribute data from an API using the injected IAttributeEndPoint service.

---
+++ WebApi/Services/AttributeItem.cs
@@ -0,0 +1,48 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public class AttributeItem : IAttribute
+    {
+        private readonly IAttributeEndPoint _services;
+
+        public AttributeItem(IApiConfigurationServices services)
+        {
+            _services = services.AttributeService;
+        }
+
+        public async Task<IEnumerable<AttributeDto>> GetAsync()
+        {
+            var response = await _services.GetAsync();
+
+            var listItemDto = await response.Content.GetListObjectAsync<AttributeDto>();
+
+            return listItemDto;
+        }
+
+        public async Task<IEnumerable<AttributeDto>> GetByItemCodeAsync(string itemCode)
+        {
+            var response = await _services.GetByItemCodeAsync(itemCode);
+
+            var listItemDto = await response.Content.GetListObjectAsync<AttributeDto>();
+
+            return listItemDto;
+        }
+
+        public async Task<IEnumerable<AttributeDto>> GetByLanguageCodeAsync(LanguageCode languageCode)
+        {
+            var response = await _services.GetByLanguageCodeAsync(languageCode.ToString());
+
+            var listItemDto = await response.Content.GetListObjectAsync<AttributeDto>();
+
+            return listItemDto;
+        }
+    }
+}
