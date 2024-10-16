6ef61450dee97a8e410dfcf2905f2aa234675c76
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-16 16:22:15 +0200

    Add IAttributeEndPoint interface for OData service
    Introduce IAttributeEndPoint interface in GtvApiHub.WebApi.EndPointInterfaces namespace using Refit library. Add three methods: GetAsync(), GetByItemCodeAsync(string itemCode), and GetByLanguageCodeAsync(string languageCode) to handle HTTP GET requests to the /odata/Attribute endpoint. Methods return Task<HttpResponseMessage> for async operations. Use AliasAs attribute for query parameter mapping.

---
+++ WebApi/EndPointInterfaces/IAttributeEndPoint.cs
@@ -0,0 +1,21 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    public interface IAttributeEndPoint
+    {
+        [Get("/odata/Attribute")]
+        Task<HttpResponseMessage> GetAsync();
+
+        [Get("/odata/Attribute?$filter=ItemCode eq '{ItemCode}'")]
+        Task<HttpResponseMessage> GetByItemCodeAsync([AliasAs("ItemCode")] string itemCode);
+
+        [Get("/odata/Attribute?$filter=LanguageCode eq '{LanguageCode}'")]
+        Task<HttpResponseMessage> GetByLanguageCodeAsync([AliasAs("LanguageCode")] string languageCode);
+    }
+}
