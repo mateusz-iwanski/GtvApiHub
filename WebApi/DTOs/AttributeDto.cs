5c83359265771cceb6382ae7794f58f459014813
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-16 16:21:08 +0200

    Add AttributeDto record with properties and interfaces
    Introduce AttributeDto.cs with necessary using directives.
    Define AttributeDto record in GtvApiHub.WebApi.DTOs namespace.
    Include properties: AttributeName, AttributeType, ItemCode,
    LanguageCode, Value, and FileHandler.
    Implement IBaseDto and IResponseDto interfaces.

---
+++ WebApi/DTOs/AttributeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.DTOs
+{
+    public record AttributeDto(
+        string AttributeName, 
+        string AttributeType, 
+        string ItemCode, 
+        string LanguageCode, 
+        string Value, 
+        string FileHandler
+        ) : IBaseDto, IResponseDto;
+}
