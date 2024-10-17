27cadf586b2245da7975c41b415ef81bf90072d7
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-17 20:20:36 +0200

    Add Item service class for GtvApi interactions
    Introduce a new Item class in GtvApiHub.WebApi.Services namespace.
    This class implements the IItem interface and provides methods
    to retrieve ItemDto objects asynchronously, either generally
    (GetAsync) or filtered by language (GetByLanguageAsync). The
    class uses IItemEndpoint for API calls.

---
+++ WebApi/Services/Item.cs
@@ -0,0 +1,45 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Extensions;
+using GtvApiHub.WebApi.Objects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Services
+{
+    /// <summary>
+    /// Represents a collection of elements in the GtvApi service
+    /// </summary>
+    public class Item : IItem
+    {
+        private readonly IItemEndpoint _services;
+
+        public Item(IApiConfigurationServices services)
+        {
+            _services = services.ItemService;
+        }
+
+        public async Task<IEnumerable<ItemDto>> GetAsync()
+        {
+            var response = await _services.GetAsync();
+
+            var listItemDto = await response.Content.GetListObjectAsync<ItemDto>();
+
+            return listItemDto;
+        }
+
+        public async Task<IEnumerable<ItemDto>> GetByLanguageAsync(LanguageCode languageCode)
+        {
+            var response = await _services.GetByLanguageAsync(languageCode.ToString());
+            
+            var listItemDto = await response.Content.GetListObjectAsync<ItemDto>();
+
+            return listItemDto;
+        }
+    }
+}
