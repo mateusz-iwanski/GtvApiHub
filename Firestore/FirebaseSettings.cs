1f6529aed21802f146a22332225cd321e4950280
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-17 20:08:11 +0200

    Add FirebaseSettings class for configuration management
    Introduced a new class `FirebaseSettings` in the `GtvApiHub.Firestore` namespace to manage Firebase configuration settings. This class includes `ProjectId` and `ApiKeyFilePath` properties, which throw `SettingsException` if accessed before being set, ensuring necessary configuration values are present in `appsettings.json`.

---
+++ Firestore/FirebaseSettings.cs
@@ -0,0 +1,32 @@
+﻿using GtvApiHub.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.Firestore
+{
+    /// <summary>
+    /// The class with the settings for the Firebase service.
+    /// 
+    /// Used to bind data from the appsettings.json file.
+    /// </summary>
+    public class FirebaseSettings
+    {
+        private string? _projectId;
+        private string? _apiKeyFilePath;
+
+        public string? ProjectId
+        {
+            get { return _projectId ?? throw new SettingsException($"In appsettings.json Firebase->Firestore->ProjectId not exists"); }
+            set => _projectId = value;
+        }
+
+        public string? ApiKeyFilePath
+        {
+            get { return _apiKeyFilePath ?? throw new SettingsException($"In appsettings.json Firebase->ApiKeyFilePath not exists"); }
+            set => _apiKeyFilePath = value;
+        }
+    }
+}
