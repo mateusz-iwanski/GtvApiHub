ad64b35a0e4e4d97ab2a9b175bb683de92d8fa09
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-17 20:13:10 +0200

    Add IFirestoreService interface for Firestore operations
    Introduce IFirestoreService with InsertDto, UpdateDto, and DeleteDto
    methods for Firestore operations. Methods are async and return a
    Task<bool>. Added necessary using directives for Firestore and DTOs.

---
+++ Firestore/IFirestoreService.cs
@@ -0,0 +1,12 @@
+﻿using Google.Cloud.Firestore;
+using GtvApiHub.WebApi.DTOs;
+
+namespace GtvApiHub.Firestore
+{
+    public interface IFirestoreService
+    {
+        Task<bool> InsertDto(IFirestoreDto dto);
+        Task<bool> UpdateDto(IFirestoreDto dto);
+        Task<bool> DeleteDto(IFirestoreDto dto);
+    }
+}
