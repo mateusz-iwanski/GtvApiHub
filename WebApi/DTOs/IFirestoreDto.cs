44ddd4db80fbca26d67d286207840fa338ee2f7d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-17 20:03:22 +0200

    Add IFirestoreDto interface for Firestore DTOs
    Introduce IFirestoreDto interface in GtvApiHub.WebApi.DTOs namespace.
    This interface includes two properties: CollectionName and DocumentUniqueField,
    which are used to specify the Firestore collection name and uniquely identify
    a document, respectively. XML comments are added for documentation.

---
+++ WebApi/DTOs/IFirestoreDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.DTOs
+{
+    /// <summary>
+    /// Firestore DTO interface.
+    /// </summary>
+    public interface IFirestoreDto
+    {
+        /// <summary>
+        /// This field is used to name the collection for the dto document in Firestore.
+        /// </summary>
+        string CollectionName { get; }
+
+        /// <summary>
+        /// This field is used to uniquely identify a document in Firestore.
+        /// The best idea is to use a field that is unique in the DTO object.
+        /// </summary>
+        string DocumentUniqueField { get; }
+    }
+}
