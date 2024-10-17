7033044ef8b2d18bab0e5b3790631b99b38a466b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-17 20:20:46 +0200

    Add IItem interface with async methods for item retrieval
    Introduced a new interface `IItem` in the `GtvApiHub.WebApi.Services` namespace.
    This interface includes two asynchronous methods:
    - `GetAsync`: Retrieves a collection of `ItemDto` objects.
    - `GetByLanguageAsync`: Retrieves a collection of `ItemDto` objects filtered by a specified `LanguageCode`.

---
+++ WebApi/Services/IItem.cs
@@ -0,0 +1,10 @@
+﻿using GtvApiHub.WebApi.DTOs;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public interface IItem
+    {
+        Task<IEnumerable<ItemDto>> GetAsync();
+        Task<IEnumerable<ItemDto>> GetByLanguageAsync(LanguageCode languageCode);
+    }
+}
