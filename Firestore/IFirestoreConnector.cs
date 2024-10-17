176c99567c388445c872cb04d335553be1659f57
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-17 20:10:20 +0200

    Add IFirestoreConnector interface for Firestore integration
    Introduced a new namespace `GtvApiHub.Firestore` and added a
    using directive for `Google.Cloud.Firestore`. Defined the
    `IFirestoreConnector` interface with methods `Connect()` and
    `GetFirestoreDb()` to establish a connection and retrieve a
    `FirestoreDb` instance.

---
+++ Firestore/IFirestoreConnector.cs
@@ -0,0 +1,10 @@
+﻿using Google.Cloud.Firestore;
+
+namespace GtvApiHub.Firestore
+{
+    public interface IFirestoreConnector
+    {
+        void Connect();
+        FirestoreDb GetFirestoreDb();
+    }
+}
