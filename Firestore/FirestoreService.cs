3e30bb8399fcc4c45a73ad81ca39c5c31f3b9735
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-17 20:12:03 +0200

    Add FirestoreService with insert and existence check
    Introduced a new `FirestoreService` class in the `GtvApiHub.Firestore` namespace, implementing the `IFirestoreService` interface. Dependencies for `FirestoreService` include `IFirestoreConnector` and `ILogger`, injected via the constructor. The constructor initializes the Firestore connection and retrieves the Firestore database instance.
    
    Added `InsertDto` method to insert a document into a Firestore collection if it does not already exist, logging the creation or existence of the document. A private helper method `IsDtoExists` checks if a document already exists in the Firestore collection. The `InsertDto` method uses `IsDtoExists` to determine if the document should be created.
    
    Utilized `Google.Cloud.Firestore` for Firestore operations and `NLog` for logging.

---
+++ Firestore/FirestoreService.cs
@@ -0,0 +1,69 @@
+﻿using Google.Cloud.Firestore;
+using GtvApiHub.WebApi.DTOs;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using NLog;
+
+namespace GtvApiHub.Firestore
+{
+    
+
+    public class FirestoreService : IFirestoreService
+    {
+        private readonly IFirestoreConnector _firestoreConnector;
+        private readonly FirestoreDb _firestoreDb;
+        private readonly ILogger _logger;
+
+        public FirestoreService(IFirestoreConnector firestoreConnector, ILogger logger)
+        {
+            _firestoreConnector = firestoreConnector;
+            _logger = logger;
+
+            _firestoreConnector.Connect();
+            _firestoreDb = firestoreConnector.GetFirestoreDb();
+        }
+
+        /// <summary>
+        /// Insert the collection (IFirestoreDto.CollectionName) if it does not exist 
+        /// with the document (the rest of the fields are marked as FirestoreProperty) if it does not exist
+        /// with the document ID (IFirestoreDto.DocumentUniqueField).
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns>true if created, false if exists</returns>
+        public async Task<bool> InsertDto(IFirestoreDto dto)
+        {
+            CollectionReference collection = _firestoreDb.Collection(dto.CollectionName);            
+            DocumentReference document = collection.Document(dto.DocumentUniqueField);
+
+            if (!await IsDtoExists(document))
+            { 
+                await collection.Document(dto.DocumentUniqueField).SetAsync(dto);
+                _logger.Info($"Firestore document '{dto.DocumentUniqueField}' in collection '{dto.CollectionName}' created.");
+                return true;
+            }
+            
+            _logger.Info($"Firestore document '{dto.DocumentUniqueField}' in collection '{dto.CollectionName}' already exists.");
+
+            return false;
+        }
+
+        
+
+
+        /// <summary>
+        /// Check if DTO object exists.
+        /// 
+        /// Checking the DTO document ID (IFirestoreDto.DocumentUniqueField) exists in the collection (IFirestoreDto.CollectionName)
+        /// </summary>
+        /// <param name="document">CollectionReference->Document(dto.DocumentUniqueField)</param>
+        /// <returns></returns>
+        private async Task<bool> IsDtoExists(DocumentReference document)
+        {
+            DocumentSnapshot snapshot = await document.GetSnapshotAsync();
+            return snapshot.Exists;
+        }
+    }
+
+
+}
