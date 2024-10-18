66b87e16650da013a6d016c24d783e4482d082d0
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-18 10:33:17 +0200

    Add IPrice interface with methods for retrieving prices
    A new interface `IPrice` has been added to the `GtvApiHub.WebApi.Services` namespace. This interface includes two method signatures:
    - `Task<IEnumerable<PriceDto>> GetAsync();` for asynchronously retrieving a collection of `PriceDto` objects.
    - `Task<IEnumerable<PriceDto>> GetByItemCodeAsync(string itemCode);` for asynchronously retrieving a collection of `PriceDto` objects filtered by the provided `itemCode`.
    
    The `GtvApiHub.WebApi.DTOs` namespace is imported to use the `PriceDto` type.

---
+++ WebApi/Services/IPrice.cs
@@ -0,0 +1,10 @@
+﻿using GtvApiHub.WebApi.DTOs;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public interface IPrice
+    {
+        Task<IEnumerable<PriceDto>> GetAsync();
+        Task<IEnumerable<PriceDto>> GetByItemCodeAsync(string itemCode);
+    }
+}
