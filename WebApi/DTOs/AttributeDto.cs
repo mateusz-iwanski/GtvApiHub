b55eeec82a34ac935857898b7db80d40ac16e5ea
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-18 11:32:15 +0200

    Refactor AttributeDto for Firestore compatibility
    Updated using directives to include Google.Cloud.Firestore and GtvApiHub.Firebase. Refactored AttributeDto from positional parameters to properties and added Firestore attributes. Implemented IFirestoreDto interface. Added CollectionName and DocumentUniqueField properties. Included XML documentation for AttributeDto.

--- WebApi/DTOs/AttributeDto.cs
+++ WebApi/DTOs/AttributeDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Google.Cloud.Firestore;
+using GtvApiHub.Firebase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +8,31 @@ using System.Threading.Tasks;
 
 namespace GtvApiHub.WebApi.DTOs
 {
-    public record AttributeDto(
-        string AttributeName, 
-        string AttributeType, 
-        string ItemCode, 
-        string LanguageCode, 
-        string Value, 
-        string FileHandler
-        ) : IBaseDto, IResponseDto;
+    /// <summary>
+    /// Attribute DTO represents product attributes from Api.
+    /// </summary>
+    [FirestoreData]
+    public record AttributeDto : IBaseDto, IResponseDto, IFirestoreDto
+    {
+        [FirestoreProperty]
+        public string AttributeName { get; init; }
+
+        [FirestoreProperty]
+        public string AttributeType { get; init; }
+
+        [FirestoreProperty]
+        public string ItemCode { get; init; }
+
+        [FirestoreProperty]
+        public string LanguageCode { get; init; }
+
+        [FirestoreProperty]
+        public string Value { get; init; }
+
+        [FirestoreProperty]
+        public string FileHandler { get; init; }
+
+        public string CollectionName { get => "itemsAttributes"; }
+        public string DocumentUniqueField { get => ItemCode; }
+    }
 }
