a274f204bf265648138cc524b4897d702cbc8d60
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-21 14:56:15 +0200

    Add UrlHelper class with GetFileNameFromUrl method
    Created UrlHelper.cs and added necessary namespaces.
    Introduced GtvApiHub.Helpers namespace.
    Added static UrlHelper class with GetFileNameFromUrl method.
    The method extracts and returns the file name from a URL.

---
+++ Helpers/UrlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.Helpers
+{
+    public static class UrlHelper
+    {
+        public static string GetFileNameFromUrl(string url)
+        {
+            Uri uri = new Uri(url);
+            string fileName = uri.Segments.Last();
+            return fileName;
+        }
+    }
+}
