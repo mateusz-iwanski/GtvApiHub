1bdab0d0bdb454c387a96813be7a81721cd5396e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-23 10:46:36 +0200

    Add ICategoryTreeEndPoint interface with Refit methods
    Introduced a new namespace `GtvApiHub.WebApi.EndPointInterfaces` and created the `ICategoryTreeEndPoint` interface. This interface includes two methods: `GetAsync`, which sends a GET request to `/odata/Category`, and `GetByItemCodeAsync`, which sends a GET request to `/odata/Category?$filter=ItemCode eq '{ItemCode}'` with a parameter `itemCode`. Added necessary using directives for `Refit`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ WebApi/EndPointInterfaces/ICategoryTreeEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    public interface ICategoryTreeEndPoint
+    {
+        [Get("/odata/Category")]
+        Task<HttpResponseMessage> GetAsync();
+
+        [Get("/odata/Category?$filter=ItemCode eq '{ItemCode}'")]
+        Task<HttpResponseMessage> GetByItemCodeAsync([AliasAs("ItemCode")] string itemCode);
+    }
+}
