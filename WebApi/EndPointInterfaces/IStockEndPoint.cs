7d6b4d91e97b01909982b690a2ecab405bbb38c4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-23 10:47:16 +0200

    Add IStockEndPoint interface for stock data retrieval
    Introduced a new interface `IStockEndPoint` within the `GtvApiHub.WebApi.EndPointInterfaces` namespace. This interface uses the Refit library to define HTTP GET requests for stock data. It includes methods `GetAsync` for retrieving all stock data and `GetByItemCodeAsync` for filtering stock data by `ItemCode`. Added necessary using directives for `Refit`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ WebApi/EndPointInterfaces/IStockEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    public interface IStockEndPoint
+    {
+        [Get("/odata/Stock")]
+        Task<HttpResponseMessage> GetAsync();
+
+        [Get("/odata/Stock?$filter=ItemCode eq '{ItemCode}'")]
+        Task<HttpResponseMessage> GetByItemCodeAsync([AliasAs("ItemCode")] string itemCode);
+    }
+}
