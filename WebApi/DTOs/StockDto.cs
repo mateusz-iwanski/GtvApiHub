6da324a6a1c2bf1d6bee90a66df50b80adecdeb8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-23 10:45:32 +0200

    Add StockDto class for Firestore integration
    Introduced a new `StockDto` class in the `GtvApiHub.WebApi.DTOs` namespace, decorated with the `[FirestoreData]` attribute for Google Firestore usage. The class implements `IBaseDto`, `IResponseDto`, and `IFirestoreDto` interfaces. It includes three properties (`WarehouseCode`, `ItemCode`, `InStock`) with `[FirestoreProperty]` attributes, and two read-only properties (`CollectionName`, `DocumentUniqueField`). Added necessary using directives for `FirebaseManager.Firestore`, `Google.Cloud.Firestore`, and `System` namespaces.

---
+++ WebApi/DTOs/StockDto.cs
@@ -0,0 +1,26 @@
+﻿using FirebaseManager.Firestore;
+using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.DTOs
+{
+    [FirestoreData]
+    public record StockDto : IBaseDto, IResponseDto, IFirestoreDto
+    {
+        [FirestoreProperty]
+        public string WarehouseCode { get; init; }
+
+        [FirestoreProperty]
+        public string ItemCode { get; init; }
+
+        [FirestoreProperty]
+        public int InStock { get; init; }
+
+        public string CollectionName { get => "ItemStock"; }
+        public string DocumentUniqueField { get => "Stock_" + WarehouseCode + "_" + ItemCode; }
+    }
+}
