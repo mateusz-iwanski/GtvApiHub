d4b26c7d660665b8c88fd2cf13b05412cb9d7ca9
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-23 10:51:16 +0200

    Add HttpContentExtensions for Firestore DTO deserialization
    Added using directives for various namespaces including FirebaseManager.Firestore, GtvApiHub.WebApi.DTOs, Newtonsoft.Json, and several System namespaces. Introduced a new namespace GtvApiHub.WebApi.Extensions and created a static class HttpContentExtensions. Added a static method GetListObjectAsync<T> to HttpContentExtensions for deserializing JSON content from HTTP responses into collections of Firestore DTOs.

---
+++ WebApi/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,31 @@
+﻿using FirebaseManager.Firestore;
+using GtvApiHub.WebApi.DTOs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Extensions
+{
+    public static class HttpContentExtensions
+    {
+        /// <summary>
+        /// Deserialize Json HttpContent from GTV Api to Firestore DTOs
+        /// </summary>
+        /// <typeparam name="T">IFirestoreDto</typeparam>
+        /// <param name="responseContent">HttpContent</param>
+        /// <returns>IEnumerable<T></returns>
+        public async static Task<IEnumerable<T>> GetListObjectAsync<T>(this HttpContent responseContent) where T : IFirestoreDto
+        {
+            var tokenResponse = await responseContent.ReadAsStringAsync();
+
+            var deserializedResponseDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(tokenResponse);
+
+            var listItemDto = JsonConvert.DeserializeObject<IEnumerable<T>>(deserializedResponseDict["value"].ToString());
+
+            return listItemDto;
+        }        
+    }
+}
