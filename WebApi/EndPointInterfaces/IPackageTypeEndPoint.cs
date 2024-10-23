935bc2cd2a49c4b7563f381f9747977427a33d05
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-23 10:46:49 +0200

    Add IPackageTypeEndPoint interface with Refit methods
    Introduced a new namespace `GtvApiHub.WebApi.EndPointInterfaces` and
    added the `IPackageTypeEndPoint` interface. This interface defines
    two methods using Refit for HTTP GET requests to an OData service:
    - `GetAsync()`: Retrieves all units.
    - `GetDefaultByItemCodeAsync(string itemCode)`: Retrieves the default
      unit for a given item code.

---
+++ WebApi/EndPointInterfaces/IPackageTypeEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    public interface IPackageTypeEndPoint
+    {
+        [Get("/odata/Unit")]
+        Task<HttpResponseMessage> GetAsync();
+
+        [Get("/odata/Unit?$filter=ItemCode eq '{ItemCode}' and isDefaultUnit eq true")]
+        Task<HttpResponseMessage> GetDefaultByItemCodeAsync([AliasAs("ItemCode")] string itemCode);
+    }
+}
