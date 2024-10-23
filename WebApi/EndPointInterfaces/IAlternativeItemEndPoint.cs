11c103cc66fd53beb84cd41855560b68db339845
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-23 10:46:22 +0200

    Add IAlternativeItemEndPoint interface with Refit methods
    - Added using directives for Refit, System, System.Collections.Generic, System.Linq, System.Text, and System.Threading.Tasks.
    - Introduced a new namespace GtvApiHub.WebApi.EndPointInterfaces.
    - Created a new interface IAlternativeItemEndPoint.
    - Defined GetAsync method to send a GET request to /odata/Alternativeitem.
    - Defined GetByBaseItemCodeAsync method to send a GET request to /odata/Alternativeitem?$filter=BaseItemCode eq '{ItemCode}'.

---
+++ WebApi/EndPointInterfaces/IAlternativeItemEndPoint.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.EndPointInterfaces
+{
+    public interface IAlternativeItemEndPoint
+    {
+        [Get("/odata/Alternativeitem")]
+        Task<HttpResponseMessage> GetAsync();
+
+        [Get("/odata/Alternativeitem?$filter=BaseItemCode eq '{ItemCode}'")]
+        Task<HttpResponseMessage> GetByBaseItemCodeAsync([AliasAs("ItemCode")] string itemCode);
+    }
+}
