0f4d56bf4aec3e6bb3b1e407b4523a1a0b0b3186
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-24 19:20:00 +0200

    Add CategoryTree service with async methods
    Introduce a new CategoryTree class in the GtvApiHub.WebApi.Services namespace, implementing the ICategoryTree interface. The class depends on ICategoryTreeEndPoint, injected via the constructor. Added two async methods: GetAsync() to fetch a list of CategoryTreeDto objects, and GetAsync(string itemCode) to fetch based on an item code. Included necessary using directives for DTOs, endpoint interfaces, extensions, and system collections.

---
+++ WebApi/Services/CategoryTree.cs
@@ -0,0 +1,41 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public class CategoryTree : ICategoryTree
+    {
+        private readonly ICategoryTreeEndPoint _services;
+
+        public CategoryTree(IApiConfigurationServices services)
+        {
+            _services = services.CategoryTreeService;
+        }
+
+        public async Task<IEnumerable<CategoryTreeDto>> GetAsync()
+        {
+            var response = await _services.GetAsync();
+
+            var listCategoryTreeDto = await response.Content.GetListObjectAsync<CategoryTreeDto>();
+
+            return listCategoryTreeDto;
+        }
+
+
+        public async Task<IEnumerable<CategoryTreeDto>> GetAsync(string itemCode)
+        {
+            var response = await _services.GetByItemCodeAsync(itemCode);
+
+            var listCategoryTreeDto = await response.Content.GetListObjectAsync<CategoryTreeDto>();
+
+            return listCategoryTreeDto;
+        }
+
+    }
+}
