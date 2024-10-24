4820e372e189493d09bc776e1fcc9655d5e7d36f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-24 19:20:16 +0200

    Add IAlternativeItem interface for alternative item retrieval
    Introduced a new namespace `GtvApiHub.WebApi.Services` and defined the `IAlternativeItem` interface with two asynchronous methods for retrieving alternative items. Added a using directive for `GtvApiHub.WebApi.DTOs` to support `AlternativeItemDto`.

---
+++ WebApi/Services/IAlternativeItem.cs
@@ -0,0 +1,10 @@
+﻿using GtvApiHub.WebApi.DTOs;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public interface IAlternativeItem
+    {
+        Task<IEnumerable<AlternativeItemDto>> GetAsync();
+        Task<IEnumerable<AlternativeItemDto>> GetAsync(string baseItemCode);
+    }
+}
