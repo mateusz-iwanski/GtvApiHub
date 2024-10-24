f0f2de67e4db76da995b51446261b98e1734ffec
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-24 19:19:24 +0200

    Add AlternativeItem class with async methods
    Introduce AlternativeItem class in GtvApiHub.WebApi.Services namespace implementing IAlternativeItem interface. Add necessary using directives. Include a private readonly field _services of type IAlternativeItemEndPoint. Add a constructor to initialize _services with AlternativeItemService. Implement two async methods: GetAsync() and GetAsync(string baseItemCode) to fetch and process AlternativeItemDto lists.

---
+++ WebApi/Services/AlternativeItem.cs
@@ -0,0 +1,39 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public class AlternativeItem : IAlternativeItem
+    {
+        private readonly IAlternativeItemEndPoint _services;
+
+        public AlternativeItem(IApiConfigurationServices services)
+        {
+            _services = services.AlternativeItemService;
+        }
+
+        public async Task<IEnumerable<AlternativeItemDto>> GetAsync()
+        {
+            var response = await _services.GetAsync();
+
+            var listItemDto = await response.Content.GetListObjectAsync<AlternativeItemDto>();
+
+            return listItemDto;
+        }
+
+        public async Task<IEnumerable<AlternativeItemDto>> GetAsync(string baseItemCode)
+        {
+            var response = await _services.GetByBaseItemCodeAsync(baseItemCode);
+
+            var listItemDto = await response.Content.GetListObjectAsync<AlternativeItemDto>();
+
+            return listItemDto;
+        }
+    }
+}
