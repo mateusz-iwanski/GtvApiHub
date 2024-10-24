8cd246688dab649e11f511f55a51800660785225
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-24 19:21:27 +0200

    Add PackageType class with async methods to fetch data
    Introduced a new class `PackageType` in the `GtvApiHub.WebApi.Services` namespace, implementing the `IPackageType` interface. The class includes:
    - A private readonly field `_services` of type `IPackageTypeEndPoint`.
    - A constructor that initializes `_services` using `IApiConfigurationServices`.
    - Two asynchronous methods:
      - `GetAsync()`: Fetches a list of `PackageTypeDto` objects.
      - `GetAsync(string itemCode)`: Fetches a list of `PackageTypeDto` objects based on an item code.
    Both methods utilize the `GetListObjectAsync<PackageTypeDto>()` extension method to process the response.

---
+++ WebApi/Services/PackageType.cs
@@ -0,0 +1,45 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Extensions;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public class PackageType : IPackageType
+    {
+        private readonly IPackageTypeEndPoint _services;
+
+        public PackageType(IApiConfigurationServices services)
+        {
+            _services = services.PackageTypeService;
+        }
+
+        public async Task<IEnumerable<PackageTypeDto>> GetAsync()
+        {
+            var response = await _services.GetAsync();
+
+            var listPackageTypeDto = await response.Content.GetListObjectAsync<PackageTypeDto>();
+
+            return listPackageTypeDto;
+        }
+
+        /// <summary>
+        /// Get default package type by item code
+        /// </summary>
+        /// <param name="itemCode">Item code</param>
+        /// <returns>IEnumerable<PackageTypeDto>></returns>
+        public async Task<IEnumerable<PackageTypeDto>> GetAsync(string itemCode)
+        {
+            var response = await _services.GetDefaultByItemCodeAsync(itemCode);
+
+            var listPackageTypeDto = await response.Content.GetListObjectAsync<PackageTypeDto>();
+
+            return listPackageTypeDto;
+        }
+    }
+}
