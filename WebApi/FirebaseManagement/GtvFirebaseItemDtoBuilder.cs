2244644b645e7be6c301ec5d9abc486053d9a647
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-24 19:18:47 +0200

    Add GtvFirebaseItemDtoBuilder for constructing Firestore DTO
    A new class `GtvFirebaseItemDtoBuilder` has been added to the
    `GtvApiHub.WebApi.FirebaseManagement` namespace. This class
    includes a private property `_firestoreItemDto` of type
    `FirestoreItemDto`. The class provides a `Build` method that
    takes several lists of DTOs (`ItemDto`, `PriceDto`, `StockDto`,
    `AttributeDto`, `CategoryTreeDto`, `PackageTypeDto`) and a
    string `itemCode`. The `Build` method filters these lists based
    on the `itemCode` and constructs a `FirestoreItemDto` object
    with the filtered data. The `Build` method returns the
    constructed `FirestoreItemDto`. Additionally, a `Get` method
    is provided to retrieve the `_firestoreItemDto` object.

---
+++ WebApi/FirebaseManagement/GtvFirebaseItemDtoBuilder.cs
@@ -0,0 +1,48 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.FirebaseManagement
+{
+    public class GtvFirebaseItemDtoBuilder
+    {
+        private FirestoreItemDto _firestoreItemDto { get; set; }
+
+        public FirestoreItemDto Build(
+            string itemCode,
+            List<ItemDto> allApiItemDtos,
+            List<PriceDto> allApiPriceDtos,
+            List<StockDto> allApiStockDtos,
+            List<AttributeDto> allApiAttributeDtos,
+            List<CategoryTreeDto> allApiCategoryTreeDtos,
+            List<PackageTypeDto> allApiPackageTypeDtos
+            )
+        {
+            List<ItemDto> items = allApiItemDtos.Where(x => x.ItemCode == itemCode).ToList();
+            List<PriceDto> prices = allApiPriceDtos.Where(x => x.ItemCode == itemCode).ToList();
+            List<StockDto> stocks = allApiStockDtos.Where(x => x.ItemCode == itemCode).ToList();
+            List<AttributeDto> attributes = allApiAttributeDtos.Where(x => x.ItemCode == itemCode).ToList();
+            List<CategoryTreeDto> categoryTrees = allApiCategoryTreeDtos.Where(x => x.ItemCode == itemCode).ToList();
+            List<PackageTypeDto> packageTypes = allApiPackageTypeDtos.Where(x => x.ItemCode == itemCode).ToList();
+
+            _firestoreItemDto = new FirestoreItemDto()
+            {
+                Item = items,
+                Price = prices.FirstOrDefault(),
+                Stocks = stocks,
+                Attributes = attributes,
+                CategoryTrees = categoryTrees,
+                PackageTypes = packageTypes,
+                ItemCode = itemCode
+            };
+
+            return _firestoreItemDto;
+        }
+
+        public FirestoreItemDto Get() => _firestoreItemDto;
+    }
+}
