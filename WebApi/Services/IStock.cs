8cdbce8dda78f6722b3a48d4a286db7aa5d9fe58
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-24 19:21:14 +0200

    Add IStock interface for async stock data retrieval
    Introduced a new namespace `GtvApiHub.WebApi.Services` and defined
    the `IStock` interface within it. The interface includes two
    asynchronous methods for retrieving stock data: `GetAsync()` and
    `GetAsync(string itemCode)`, both returning `Task<IEnumerable<StockDto>>`.
    Also added a using directive for `GtvApiHub.WebApi.DTOs` in `IStock.cs`.

---
+++ WebApi/Services/IStock.cs
@@ -0,0 +1,10 @@
+﻿using GtvApiHub.WebApi.DTOs;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public interface IStock
+    {
+        Task<IEnumerable<StockDto>> GetAsync();
+        Task<IEnumerable<StockDto>> GetAsync(string itemCode);
+    }
+}
