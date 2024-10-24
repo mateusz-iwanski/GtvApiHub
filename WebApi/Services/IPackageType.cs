4d06a51e538f3aae7ca538bdd829dafafa99ee82
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-24 19:20:38 +0200

    Add IPackageType interface for package type retrieval
    Introduced a new IPackageType interface in the GtvApiHub.WebApi.Services namespace. This interface includes two asynchronous methods for retrieving package types, one with and one without an itemCode parameter. Also added a using directive for GtvApiHub.WebApi.DTOs to utilize PackageTypeDto.

---
+++ WebApi/Services/IPackageType.cs
@@ -0,0 +1,10 @@
+﻿using GtvApiHub.WebApi.DTOs;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public interface IPackageType
+    {
+        Task<IEnumerable<PackageTypeDto>> GetAsync();
+        Task<IEnumerable<PackageTypeDto>> GetAsync(string itemCode);
+    }
+}
