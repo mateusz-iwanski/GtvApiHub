7b2d9f928d7a9d2f49333a96b5b9e4a54d928c98
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-25 18:38:12 +0200

    Add optional params to StoreAsync in IFirestorageFileHandler
    The StoreAsync method in the IFirestorageFileHandler interface
    now includes two optional parameters: fileUrlPrefix and
    directoryNameOnFirestore. The method signature has changed from
    Task<bool> StoreAsync(IStorageStrategy dto); to Task<bool>
    StoreAsync(IStorageStrategy dto, string? fileUrlPrefix = null,
    string? directoryNameOnFirestore = null);. This change allows
    for additional customization and flexibility in file storage.

--- WebApi/Services/IFirestorageFileHandler.cs
+++ WebApi/Services/IFirestorageFileHandler.cs
@@ -4,6 +4,6 @@ namespace GtvApiHub.WebApi.Services
 {
     public interface IFirestorageFileHandler
     {
-        Task<bool> StoreAsync(IStorageStrategy dto);
+        Task<bool> StoreAsync(IStorageStrategy dto, string? fileUrlPrefix = null, string? directoryNameOnFirestore = null);
     }
 }
