927ef7767f34a6b2bd83b1184f5471428a3c032d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-25 18:39:28 +0200

    Add Stock service class implementing IStock interface
    Introduce a new Stock class in the GtvApiHub.WebApi.Services namespace.
    This class implements the IStock interface and provides two asynchronous
    methods, GetAsync() and GetAsync(string itemCode), to fetch stock data.
    The class interacts with an IStockEndPoint service to retrieve and process
    the data into a list of StockDto objects.

---
+++ WebApi/Services/Stock.cs
@@ -0,0 +1,39 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public class Stock : IStock
+    {
+        private readonly IStockEndPoint _services;
+
+        public Stock(IApiConfigurationServices services)
+        {
+            _services = services.StockService;
+        }
+
+        public async Task<IEnumerable<StockDto>> GetAsync()
+        {
+            var response = await _services.GetAsync();
+
+            var listStockDto = await response.Content.GetListObjectAsync<StockDto>();
+
+            return listStockDto;
+        }
+
+        public async Task<IEnumerable<StockDto>> GetAsync(string itemCode)
+        {
+            var response = await _services.GetByItemCodeAsync(itemCode);
+
+            var listStockDto = await response.Content.GetListObjectAsync<StockDto>();
+
+            return listStockDto;
+        }
+    }
+}
