f94869a670e39cb33b7add5a86be231536b2efa0
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-25 18:39:08 +0200

    Add Promotion class with async GetAsync method
    Introduce a new Promotion class in the GtvApiHub.WebApi.Services namespace, implementing the IPromotion interface. The class includes a private readonly field _services of type IPromotionEndPoint, initialized via the constructor with a PromotionService from IApiConfigurationServices. Added an asynchronous GetAsync method to fetch and return a list of PromotionDto objects. Included necessary using directives for DTOs, endpoint interfaces, extensions, and system collections.

---
+++ WebApi/Services/Promotion.cs
@@ -0,0 +1,30 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using GtvApiHub.WebApi.EndPointInterfaces;
+using GtvApiHub.WebApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.Services
+{
+    public class Promotion : IPromotion
+    {
+        private readonly IPromotionEndPoint _services;
+
+        public Promotion(IApiConfigurationServices services)
+        {
+            _services = services.PromotionService;
+        }
+
+        public async Task<IEnumerable<PromotionDto>> GetAsync()
+        {
+            var response = await _services.GetAsync();
+
+            var listItemDto = await response.Content.GetListObjectAsync<PromotionDto>();
+
+            return listItemDto;
+        }
+    }
+}
