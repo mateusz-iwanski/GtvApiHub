34e30ea26e596fcdaad40c5461f67a1a77f6f3d1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-25 18:39:48 +0200

    Add new services to IApiConfigurationServices
    Introduced new services to IApiConfigurationServices and its implementation ApiConfigurationServices:
    - IPackageTypeEndPoint PackageTypeService
    - IAlternativeItemEndPoint AlternativeItemService
    - IStockEndPoint StockService
    - ICategoryTreeEndPoint CategoryTreeService
    - IPromotionEndPoint PromotionService
    
    These services are initialized in the constructor using RestService.For<T>.

--- WebApi/ApiConfigurationServices.cs
+++ WebApi/ApiConfigurationServices.cs
@@ -12,12 +12,20 @@ using System.Threading.Tasks;
 
 namespace GtvApiHub.WebApi
 {
+    /// <summary>
+    /// Refit configuration of the specified interface.
+    /// </summary>
     public interface IApiConfigurationServices
     {
         ITokenEndPoint TokenService { get; }
         IItemEndpoint ItemService { get; }
         IPriceEndPoint PriceService { get; }
         IAttributeEndPoint AttributeService { get; }
+        IPackageTypeEndPoint PackageTypeService { get; }
+        IAlternativeItemEndPoint AlternativeItemService { get; }
+        IStockEndPoint StockService { get; }
+        ICategoryTreeEndPoint CategoryTreeService { get; }
+        IPromotionEndPoint PromotionService { get; }
     }
 
     public class ApiConfigurationServices : ApiConfiguration, IApiConfigurationServices
@@ -26,6 +34,11 @@ namespace GtvApiHub.WebApi
         public IItemEndpoint ItemService { get; private set; }
         public IPriceEndPoint PriceService { get; private set; }
         public IAttributeEndPoint AttributeService { get; private set; }
+        public IPackageTypeEndPoint PackageTypeService { get; private set; }
+        public IAlternativeItemEndPoint AlternativeItemService { get; private set; }
+        public IStockEndPoint StockService { get; private set; }
+        public ICategoryTreeEndPoint CategoryTreeService { get; private set; }
+        public IPromotionEndPoint PromotionService { get; private set; }
 
         public ApiConfigurationServices(IOptions<GtvApiSettings> gtvApiSettings, IServiceProvider serviceProvider) : base(gtvApiSettings, serviceProvider)
         {
@@ -33,6 +46,11 @@ namespace GtvApiHub.WebApi
             ItemService = RestService.For<IItemEndpoint>(httpClient);
             PriceService = RestService.For<IPriceEndPoint>(httpClient);
             AttributeService = RestService.For<IAttributeEndPoint>(httpClient);
+            PackageTypeService = RestService.For<IPackageTypeEndPoint>(httpClient);
+            AlternativeItemService = RestService.For<IAlternativeItemEndPoint>(httpClient);
+            StockService = RestService.For<IStockEndPoint>(httpClient);
+            CategoryTreeService = RestService.For<ICategoryTreeEndPoint>(httpClient);
+            PromotionService = RestService.For<IPromotionEndPoint>(httpClient);
         }
     }
 }
