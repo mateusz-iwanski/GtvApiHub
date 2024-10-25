dcf92b6849d2ecf9fc80a128a122bb1410ba86e3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-25 18:37:55 +0200

    Refactor FirestorageDtoFileHandler for flexibility
    Removed IOptions<GtvApiSettings> dependency from FirestorageDtoFileHandler. Updated constructor to remove gtvApiSettings parameter. Modified StoreAsync method to include optional fileUrlPrefix and directoryNameOnFirestore parameters. Updated logic to handle directory creation and file naming based on these parameters.

--- WebApi/Services/FirestorageDtoFileHandler.cs
+++ WebApi/Services/FirestorageDtoFileHandler.cs
@@ -23,18 +23,15 @@ namespace GtvApiHub.WebApi.Services
     {
         private readonly IFirestorageService _firestorageService;
         private readonly IOptions<FirebaseSettings> _firebaseSettings;
-        private readonly IOptions<GtvApiSettings> _gtvApiSettings;
         private readonly ILogger _logger;
 
         public FirestorageDtoFileHandler(
             IFirestorageService firestorageService, 
             IOptions<FirebaseSettings> settings, 
-            IOptions<GtvApiSettings> gtvApiSettings,
             ILogger logger)
         {
             _firestorageService = firestorageService;
             _firebaseSettings = settings;
-            _gtvApiSettings = gtvApiSettings;
             _logger = logger;
         }
 
@@ -42,15 +39,17 @@ namespace GtvApiHub.WebApi.Services
         /// Download and upload file to Firebase Storage from DTO.
         /// </summary>
         /// <param name="dto">IStorageStrategy</param>
+        /// <param name="fileUrlPrefix">If you want to add prefix to file url</param>
+        /// <param name="directoryNameOnFirestore">If you want to store file in directory on Firebase Storage</param>
         /// <returns>If DTO doesn't have file return false, true if store correctly</returns>
-        public async Task<bool> StoreAsync(IStorageStrategy dto)
+        public async Task<bool> StoreAsync(IStorageStrategy dto, string? fileUrlPrefix = null, string? directoryNameOnFirestore = null)
         {
             var urlFile = dto.GetFilePath();
             if (urlFile == null)
                 return false;
 
             urlFile = Path.Combine(
-                _gtvApiSettings.Value.FileUrlPrefix,                
+                fileUrlPrefix,
                 dto.GetFilePath()
                 );            
 
@@ -61,15 +60,26 @@ namespace GtvApiHub.WebApi.Services
                 Directory.CreateDirectory(directoryPath);
             }
 
-            var fileName = UrlHelper.GetFileNameFromUrl(urlFile);
+            // Get only file name
+            var fileNameFromUrl = UrlHelper.GetFileNameFromUrl(urlFile);
 
+            // File name with prefix with directory name on Firebase storage
+            var fileName = fileNameFromUrl;
+            if (directoryNameOnFirestore != null)
+            {
+                // Path.Combine set backslashes, but they must be slashes in the path
+                fileName = $"{directoryNameOnFirestore}/{fileNameFromUrl}";
+                await _firestorageService.CreateDirectoryAsync(directoryNameOnFirestore);
+            }
+
+            // file name with prefix with directory name on local path
             string filePathToSave = Path.Combine(
                 AppDomain.CurrentDomain.BaseDirectory,
                 _firebaseSettings.Value.LocalDirectoryForfileToDownload,
-                fileName
+                fileNameFromUrl
                 );
 
-            await new HttpFileHandler(_logger).DownloadFileAsync(urlFile, filePathToSave);
+            await new HttpFileHandler(_logger).DownloadFileAsync(urlFile, filePathToSave);            
 
             await _firestorageService.UploadFileAsync(filePathToSave, fileName);
 
