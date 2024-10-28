45d394c2f1e45ea0950f29e5089b8dd62c0f051f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-28 19:05:25 +0100

    Add WarehouseCode enum and necessary using directives
    Introduced a new `WarehouseCode` enum in the `GtvApiHub.WebApi` namespace to represent different warehouse types: `M_MLP` (default) and `M_PROFIL` (long elements). Added `using` directives for `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ WebApi/WarehouseCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi
+{
+    public enum WarehouseCode
+    {
+        M_MLP,  // default warehouse
+        M_PROFIL  // warehouse with long elements
+    }
+}
