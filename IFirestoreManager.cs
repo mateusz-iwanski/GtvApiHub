bb9044fa27a95cd3861b016495ac4c18672ccf1a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-28 19:06:21 +0100

    Add IFirestoreManager interface with SyncFirestoreAsync method
    Introduced a new interface `IFirestoreManager` within the `GtvApiHub`
    namespace. This interface includes the asynchronous method
    `SyncFirestoreAsync` with two optional boolean parameters:
    `continueOnError` and `skipApiNullData`. Added necessary `using`
    directives for required namespaces.

---
+++ IFirestoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GtvApiHub
+{
+    public interface IFirestoreManager
+    {
+        Task SyncFirestoreAsync(bool continueOnError, bool skipApiNullData = true);
+    }
+}
