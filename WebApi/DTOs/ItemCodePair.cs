21d2ce08198f35d9884641bb65190016bdd13035
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-11-04 09:44:20 +0100

    Add ItemCodePair record for Firestore integration
    Introduce ItemCodePair.cs defining ItemCodePair record in GtvApiHub.WebApi.DTOs namespace. Annotate properties with Firestore attributes for Firestore field mapping. Include necessary using directives.

---
+++ WebApi/DTOs/ItemCodePair.cs
@@ -0,0 +1,22 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.DTOs
+{
+    /// <summary>
+    /// Used in ContentsDto
+    /// </summary>
+    [FirestoreData]
+    public record ItemCodePair
+    {
+        [FirestoreProperty]
+        public int Id { get; init; }
+
+        [FirestoreProperty]
+        public string ItemCode { get; init; }
+    }
+}
