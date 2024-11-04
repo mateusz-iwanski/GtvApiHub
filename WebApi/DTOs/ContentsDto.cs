3e8f3c4e2b6ec8f963f11ad40384b3faafc1d8b4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-11-04 09:44:00 +0100

    Add ContentsDto class for efficient Firestore searches
    Introduce ContentsDto in GtvApiHub.WebApi.DTOs to store document IDs and item codes, enhancing Firestore search efficiency. The class uses Firestore attributes and implements IBaseDto, IResponseDto, and IFirestoreDto interfaces.

---
+++ WebApi/DTOs/ContentsDto.cs
@@ -0,0 +1,29 @@
+﻿using FirebaseManager.Firestore;
+using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.DTOs
+{
+    /// <summary>
+    /// Store the id documents with the item code inside documents.
+    /// 
+    /// If you want to find item code or document ID, you do not have to search all documents,
+    /// just find it over id or item code from ContentsDto and read just one document.
+    /// Firestore has reading limits so the best option is using this DTO to find object over
+    /// item code or document ID.
+    /// </summary>
+    [FirestoreData]
+    public class ContentsDto : IBaseDto, IResponseDto, IFirestoreDto
+    {
+        [FirestoreProperty]
+        public List<ItemCodePair> ItemCodePairs { get; init; }
+
+        public string CollectionName => "Gtv_Items_Contents";
+
+        public string DocumentUniqueField => "Contents";
+    }
+}
