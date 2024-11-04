f2c9d8269655114a35e911b44b6b8192a8f80fc3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-11-04 09:44:32 +0100

    Add FirestoreContentsDtoBuilder class for DTO construction
    A new class `FirestoreContentsDtoBuilder` has been added to the
    `GtvApiHub.WebApi.FirebaseManagement` namespace. This class
    includes a private property `_contentsDto` of type `ContentsDto`
    and a `Build` method that constructs a list of `ItemCodePair`
    objects from a list of `FirestoreItemDto` objects. The `_contentsDto`
    property is initialized with this list and returned by the `Build`
    method. Additionally, several namespaces have been imported to
    support these changes.

---
+++ WebApi/FirebaseManagement/FirestoreContentsDtoBuilder.cs
@@ -0,0 +1,31 @@
+﻿using GtvApiHub.WebApi.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.WebApi.FirebaseManagement
+{
+    public class FirestoreContentsDtoBuilder
+    {
+        private ContentsDto _contentsDto { get; set; }
+
+        public ContentsDto Build(List<FirestoreItemDto> allApiItemDtos)
+        {
+            List<ItemCodePair> ItemCodePairs = new List<ItemCodePair>();
+
+            foreach (var item in allApiItemDtos)
+            {
+                ItemCodePairs.Add(new ItemCodePair { Id = item.Id, ItemCode = item.ItemCode });
+            }
+
+            _contentsDto = new ContentsDto()
+            {
+                ItemCodePairs = ItemCodePairs
+            };
+
+            return _contentsDto;
+        }
+    }
+}
