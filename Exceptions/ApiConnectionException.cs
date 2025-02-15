5fcca1d3aa4befa3886c2b8341675193607d12aa
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-15 16:26:35 +0100

    Add ApiConnectionException class for API error handling
    Introduce internal ApiConnectionException class in GtvApiHub.Exceptions namespace. This class inherits from CustomException and includes two constructors for handling API connection-related errors. Add necessary using directives for System, System.Collections.Generic, System.Linq, System.Text, and System.Threading.Tasks.

---
+++ Exceptions/ApiConnectionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtvApiHub.Exceptions
+{
+    internal class ApiConnectionException : CustomException
+    {
+        public ApiConnectionException(string message) : base(message)
+        {
+        }
+
+        public ApiConnectionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
